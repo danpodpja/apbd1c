beb4915b31a37e08d84fb678ccddac349c61f681
Daniel Podstawka <s29592@pjwstk.edu.pl>
2025-03-02 11:22:34 +0100

    Modyfikacja 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (int i = 0; i < 100; i+=6)
 {
+    if (i % 6 == 0) i--;
     Console.Write(i + '\t');
 }
 Console.WriteLine("Hello, World!");
